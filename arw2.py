f27fc057f37675415c14daa1b5eccb3d0f8aac44
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-07 00:12:08 +0300

    .

--- arw2.py
+++ arw2.py
@@ -243,7 +243,7 @@ def read_data():
     # ---
     c = 0
     # ---
-    with bz2.open(filename, "r") as f:
+    with bz2.open(filename, "r", encoding="utf-8") as f:
         for line in f:
             line = line.decode("utf-8").strip("\n").strip(",")
             if line.startswith('{') and line.endswith('}'):
