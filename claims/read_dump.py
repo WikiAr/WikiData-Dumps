d3ff9459bfcb4eeb20ced7304aef65fdb6e33441
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-01-03 02:31:50 +0300

    Update read_dump.py

--- claims/read_dump.py
+++ claims/read_dump.py
@@ -5,14 +5,16 @@ python3 wd_core/dump/claims/read_dump.py test
 https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2
 
 """
+import bz2
+import codecs
+import json
 import os
 import sys
-import codecs
-import bz2
-import json
 import time
 from datetime import datetime
 from pathlib import Path
+
+from dump.memory import print_memory
 
 # ---
 time_start = time.time()
@@ -28,16 +30,16 @@ print(f'sys.path.append:core_dir: {core_dir}')
 # ---
 from dump.memory import print_memory
 
-# from dump.claims.fix_dump import fix_props
+
 # ---
 filename = "/mnt/nfs/dumps-clouddumps1002.wikimedia.org/other/wikibase/wikidatawiki/latest-all.json.bz2"
 # ---
-Dump_Dir = "/data/project/himo/dumps"
+dump_dir = "/data/project/himo/dumps"
 # ---
 if os.path.exists(r'I:\core\dumps'):
-    Dump_Dir = r'I:\core\dumps'
+    dump_dir = r'I:\core\dumps'
 # ---
-print(f'Dump_Dir:{Dump_Dir}')
+print(f'dump_dir: {dump_dir}')
 # ---
 test_limit = {1: 15000}
 # ---
@@ -62,10 +64,10 @@ tab = {
 
 
 def log_dump(tab, _claims="claims"):
-    jsonname = f"{Dump_Dir}/{_claims}.json"
+    jsonname = f"{dump_dir}/{_claims}.json"
     # ---
     if 'test' in sys.argv:
-        jsonname = f"{Dump_Dir}/{_claims}_test.json"
+        jsonname = f"{dump_dir}/{_claims}_test.json"
     # ---
     with open(jsonname, "w", encoding='utf-8') as outfile:
         json.dump(tab, outfile)
@@ -101,37 +103,37 @@ def read_file(mode="rt"):
         print(f"file {filename} not found")
         return {}
 
-    t1 = time.time()
+    start_time = time.time()
     tab['file_date'] = get_file_info(filename)
     print(f"file date: {tab['file_date']}")
 
     print(f"file {filename} found, read it:")
-    c = 0
+    count = 0
     # ---
     check_file_date(tab['file_date'])
     # ---
-    # with bz2.open(filename, mode, encoding="utf-8") as f:
-    with bz2.open(filename, mode, encoding="utf-8") as f:
-        for line in f:
+
+    with bz2.open(filename, mode, encoding="utf-8") as file:
+        for line in file:
             line = line.decode("utf-8").strip("\n").strip(",")
-            tab['done'] += 1
+            tab['done'] += 1  # Counts the number of lines processed
             # ---
             if 'pp' in sys.argv:
                 print(line)
             # ---
             if line.startswith("{") and line.endswith("}"):
-                tab['All_items'] += 1
-                c += 1
+                tab['All_items'] += 1  # Increment the count of all processed items
+                count += 1
                 if 'test' in sys.argv:
-                    if c % 100 == 0:
-                        print(f'c:{c}')
+                    if count % 100 == 0:
+                        print(f'count:{count}')
                         print(f"done:{tab['done']}")
                         # ---
-                        print(c, time.time() - t1)
-                        t1 = time.time()
+                        print(count, time.time() - start_time)
+                        start_time = time.time()
 
-                    if c > test_limit[1]:
-                        print('c>test_limit[1]')
+                    if count > test_limit[1]:
+                        print('count>test_limit[1]')
                         break
 
                 json1 = json.loads(line)
@@ -150,60 +152,58 @@ def read_file(mode="rt"):
                     # ---
                     claims_example = {"claims": {"P31": [{"mainsnak": {"snaktype": "value", "property": "P31", "hash": "b44ad788a05b4c1b2915ce0292541c6bdb27d43a", "datavalue": {"value": {"entity-type": "item", "numeric-id": 6256, "id": "Q6256"}, "type": "wikibase-entityid"}, "datatype": "wikibase-item"}, "type": "statement", "id": "Q805$81609644-2962-427A-BE11-08BC47E34C44", "rank": "normal"}]}}
                     # ---
-                    for p in claims.keys():
-                        Type = claims[p][0].get("mainsnak", {}).get("datatype", '')
+                    for property in claims.keys():
+                        Type = claims[property][0].get("mainsnak", {}).get("datatype", '')
                         # ---
                         if Type == "wikibase-item":
-                            if p not in tab['properties']:
-                                tab['properties'][p] = {
+                            if property not in tab['properties']:
+                                tab['properties'][property] = {
                                     "qids": {"others": 0},
                                     "lenth_of_usage": 0,
                                     "len_prop_claims": 0,
                                 }
-                            tab['properties'][p]["lenth_of_usage"] += 1
-                            tab['all_claims_2020'] += len(claims[p])
+                            tab['properties'][property]["lenth_of_usage"] += 1
+                            tab['all_claims_2020'] += len(claims[property])
                             # ---
-                            for claim in claims[p]:
-                                tab['properties'][p]["len_prop_claims"] += 1
+                            for claim in claims[property]:
+                                tab['properties'][property]["len_prop_claims"] += 1
                                 # ---
                                 datavalue = claim.get("mainsnak", {}).get("datavalue", {})
-                                # ttype = datavalue.get("type")
-                                # ---
-                                # print(f"ttype:{ttype}")
-                                # ---
-                                # if ttype == "wikibase-entityid":
+
                                 idd = datavalue.get("value", {}).get("id")
                                 # ---
                                 if idd:
-                                    if idd not in tab['properties'][p]["qids"]:
-                                        tab['properties'][p]["qids"][idd] = 1
+                                    if idd not in tab['properties'][property]["qids"]:
+                                        tab['properties'][property]["qids"][idd] = 1
                                     else:
-                                        tab['properties'][p]["qids"][idd] += 1
+                                        tab['properties'][property]["qids"][idd] += 1
                                 # ---
                                 del idd
                                 # ---
                                 del datavalue
-                                # del ttype
+
                 # ---
                 del json1
                 del claims
             # ---
-            if (c % 1000 == 0 and c < 100000) or c % 100000 == 0:
-                print(c, time.time() - t1)
-                t1 = time.time()
+            if (count % 1000 == 0 and count < 100000) or count % 100000 == 0:
+                print(count, time.time() - start_time)
+                start_time = time.time()
                 # print memory usage
                 print_memory()
-                if c % 1000000 == 0:
+                if count % 1000000 == 0:
                     log_dump(tab)
             # ---
     # ---
     print(f"read all lines: {tab['done']}")
     # ---
-    for x, xx in tab['properties'].copy().items():
-        tab['properties'][x]["len_of_qids"] = len(xx["qids"])
-        # tab['properties'][x]["qids"] = {k: v for k, v in sorted(xx['qids'].items(), key=lambda item: item[1], reverse=True)}
+    for property_key, property_value in tab['properties'].copy().items():
+        tab['properties'][property_key]["len_of_qids"] = len(property_value["qids"])
+        # Sort QIDs by their count in descending order and update in the 'tab' dictionary
+        #sorted_qids = sorted(property_value['qids'].items(), key=lambda item: item[1], reverse=True)
+        #tab['properties'][property_key]["qids"] = dict(sorted_qids)
     # ---
-    tab['len_all_props'] = len(tab['properties'])
+    tab['len_all_props'] = len(tab['properties'])  # Calculate the total number of unique properties processed
     # ---
     end = time.time()
     # ---
