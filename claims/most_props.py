956e7344ade4cb569aea96fd6b77cb4e7dc52b6e
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-04-05 00:15:30 +0300

    Update most_props.py

--- claims/most_props.py
+++ claims/most_props.py
@@ -1,32 +1,29 @@
-
 import re
 import json
-
 from pathlib import Path
 from newapi.page import MainPage
 
-Dir = Path(__file__). parent
-
-file = Dir / "properties.json"
 def get_most_usage(text):
-	
     properties = {}
-    lines = text.split('\n')
-    for line in lines:
+    for line in text.split('\n'):
         match = re.match(r'\|(\d+)=(\d+)', line)
         if match:
             t1, t2 = match.groups()
             properties[f"P{t1}"] = int(t2)
     sorted_properties = sorted(properties.items(), key=lambda x: x[1], reverse=True)
     return dict(sorted_properties[:500])
-    
-def log_data(data):
-	with open(file, "w") as f:
-		json.dump(data, f)
-		
+
+def log_data(data, file_path):
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
 def main():
+    file_path = Path(__file__).parent / "properties.json"
     title = "Template:Number of main statements by property"
-    page      = MainPage(title, 'www', family='wikidata')
-    text        = page.get_text()
+    page = MainPage(title, 'www', family='wikidata')
+    text = page.get_text()
     data = get_most_usage(text)
-    log_data(data)
+    log_data(data, file_path)
+
+if __name__ == "__main__":
+    main()
