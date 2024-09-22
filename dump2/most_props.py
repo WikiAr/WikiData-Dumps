54d352aca7a8ee3e213c4619ddc08b42e74f572c
ibrahem <ibrahem.al-radaei@outlook.com>
2024-09-23 02:50:42 +0300

    1

--- dump2/most_props.py
+++ dump2/most_props.py
@@ -11,6 +11,9 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 
 def get_query_result(query):
+    
+    # TODO: https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/WDQS_graph_split/Rules#Scholarly_Articles
+
     endpoint_url = "https://query.wikidata.org/sparql"
     # ---
     user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
