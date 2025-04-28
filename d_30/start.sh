2f3b2359490537b5e0d2448ccde6fd4d4444c6bc
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-28 03:53:34 +0300

    Update start.sh

--- d_30/start.sh
+++ d_30/start.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-# wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/start.sh -O start.sh
 # sh start.sh
 
 wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/most_props.py -O most_props.py
-
 wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/web2.py -O web2.py
-
 wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/requirements.in -O requirements.in
 
 pip install -r requirements.in
@@ -13,5 +9,3 @@ pip install -r requirements.in
 python3 most_props.py
 
 python3 web2.py from_url
-
-echo "Finishing dump26..."
