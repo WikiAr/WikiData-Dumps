7894bd6c771d921108bf0554788cf86ce895e0ab
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-28 03:48:16 +0300

    Update start.sh

--- d_30/start.sh
+++ d_30/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/start.sh -O start.sh
+# sh start.sh
 
 wget https://raw.githubusercontent.com/WikiAr/WikiData-Dumps/refs/heads/update/d_30/most_props.py -O most_props.py
 
